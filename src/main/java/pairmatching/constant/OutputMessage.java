4d28868ca7f1680768a6ea7d68988e5560c64825
hectick <chyeon1223@cau.ac.kr>
2022-12-14 22:43:09 +0900

    feat: 매칭이력이 없다고 출력하는 기능 추가

---
+++ src/main/java/pairmatching/constant/OutputMessage.java
@@ -0,0 +1,6 @@
+package pairmatching.constant;
+
+public class OutputMessage {
+
+    public static final String NO_MATCHING_HISTORY_MESSAGE = "[ERROR] 매칭 이력이 없습니다.";
+}
--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,5 +1,6 @@
 package pairmatching.view;
 
+import pairmatching.constant.OutputMessage;
 import pairmatching.model.Pair;
 
 import java.util.List;
@@ -36,12 +37,16 @@ public class OutputView {
     }
 
     public void printRematchingMessage(){
-        System.out.println("매칭 정보가 있습니다. 다시 매칭하시겠습니가?");
+        System.out.println("매칭 정보가 있습니다. 다시 매칭하시겠습니까?");
         System.out.println("네 | 아니오");
     }
 
     public void printInitializeMessage(){
         System.out.println("초기화 되었습니다.");
+    }
+
+    public void printNoMatchingHistoryMessage(){
+        System.out.println(OutputMessage.NO_MATCHING_HISTORY_MESSAGE);
     }
 
 }
