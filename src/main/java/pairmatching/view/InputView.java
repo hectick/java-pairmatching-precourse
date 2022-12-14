bafb5c5d1758756345731f09ad99c5e7c3113628
hectick <chyeon1223@cau.ac.kr>
2022-12-14 20:22:14 +0900

    feat: 기능을 선택하는 기능 추가

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,11 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String readFunctionSelect(){
+        String input = Console.readLine();
+        return input;
+    }
+}
