cdbdc01ce3d588c61b5ec602998404bf7051d2b3
hectick <chyeon1223@cau.ac.kr>
2022-12-14 22:56:29 +0900

    feat: 매칭/조회하고자 하는 과정, 레벨, 미션 입력받는 기능 추가

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -2,6 +2,9 @@ package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputView {
 
     public String readFunctionSelect(){
@@ -12,6 +15,21 @@ public class InputView {
     public String readRematchingSelect(){
         String input = Console.readLine();
         return input;
+    }
+
+    public List<String> readCourseAndLevelAndMissionSelects(){
+        String line = Console.readLine();
+        List<String> inputs = parse(line);
+        return inputs;
+    }
+
+    private List<String> parse(String line){
+        List<String> words = new ArrayList<>();
+        String[] array = line.split(",");
+        for(int i = 0; i < array.length; i++){
+            words.add(array[i].trim());
+        }
+        return words;
     }
 
 
