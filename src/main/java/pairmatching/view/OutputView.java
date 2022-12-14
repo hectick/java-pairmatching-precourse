8dd86168f5abd5f28c55dda89572af6c2dbeeae3
hectick <chyeon1223@cau.ac.kr>
2022-12-14 22:37:40 +0900

    feat: 재매칭 안내문구를 출력하는 기능 추가

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -35,6 +35,9 @@ public class OutputView {
         System.out.println("ex) 백엔드, 레벨1, 자동차경주");
     }
 
-
+    public void printRematchingMessage(){
+        System.out.println("매칭 정보가 있습니다. 다시 매칭하시겠습니가?");
+        System.out.println("네 | 아니오");
+    }
 
 }
